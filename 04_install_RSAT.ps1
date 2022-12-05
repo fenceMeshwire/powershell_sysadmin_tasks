a1d037477e69ccd53127dc089f5db485328cb774
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-12-06 00:08:42 +0100

    Update 04_install_RSAT.ps1

--- 04_install_RSAT.ps1
+++ 04_install_RSAT.ps1
@@ -1,3 +1,5 @@
+# RSAT: Remote-Server-Administration-Tools
+
 Get-WindowsCapability -Name RSAT* -Online | Select-Object -Property DisplayName, State
 
 Add-WindowsCapability -Online -Name Rsat.ServerManager.Tools~~~~0.0.1.0
