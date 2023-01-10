117f0e08fd5b6e4bdc84338d4753e282628dd149
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-01-10 11:10:16 +0100

    Update 99_misc.ps1

--- 99_misc.ps1
+++ 99_misc.ps1
@@ -7,3 +7,7 @@ start C:\temp
 #____________________________________________________________
 # Get architecture (32/64 Bit)
 wmic OS get OSArchitecture
+
+#____________________________________________________________
+# List directories in a directory:
+Get-ChildItem C:\Users\ -directory
