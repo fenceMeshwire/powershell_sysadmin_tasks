05c959629c2c6e403595509ee660771a51211f41
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-01-22 10:13:56 +0100

    Update and rename 06_downgrade_domain_controller.ps1 to 10_remove_ad_ds_from_machine.ps1

--- 10_remove_ad_ds_from_machine.ps1
+++ 10_remove_ad_ds_from_machine.ps1
@@ -1,3 +1,5 @@
+# PowerShell Script to remove AD DS from the machine (AD DS = Active Directory Domain Services):
+
 Import-Module ADDSDeployment
 Uninstall-ADDSDomainController `
 -DemoteOperationMasterRole:$true `
