2f6b1faeade46d79dc9bc83bd1f2ff88582ef14d
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-01-27 07:28:40 +0100

    Create 11_ADuser_commands.ps1

---
+++ 11_ADuser_commands.ps1
@@ -0,0 +1,22 @@
+# Obtain user information, including SID
+
+$identity = Get-ADUser -Identity 'YOUR-USER'
+$identity                     # Returns all information
+$identity.Name                # Returns the Name
+$identity.SID                 # Returns the SID
+$identity.UserPrincipalName   # Returns username@domain
+
+# Obtain user information by using the SID
+# SID: S-X-X-XX-XXXXXXXXX-XXXXXXXXXX-XXXXXXX-XXXX
+$identity = Get-ADUser -Identity 'SID'
+$identity                     # Returns all information
+$identity.Name                # Returns the Name
+$identity.SID                 # Returns the SID
+$identity.UserPrincipalName   # Returns username@domain
+
+# Delete local user:
+Remove-Aduser username -Confirm:$False    # Delete the username without confirmation
+                                          # Note: write username as is, no declaration
+# Create local user:
+$password = Read-Host -AsSecureString
+New-ADUser -Name "user" -Accountpassword $password -OtherAttributes @{'title'="worker";'mail'="user@company.com"} -PasswordNotRequired:$False
