70ce40ea0a1a05d3e337c8015bf162c674cbcc8f
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-01-28 09:25:49 +0100

    Update 11_ADuser_commands.ps1

--- 11_ADuser_commands.ps1
+++ 11_ADuser_commands.ps1
@@ -1,3 +1,10 @@
+# List all AD users:
+Get-ADUser -Filter *
+# _____________________________________________________________________
+# Search for a particular user "SampleS":
+Get-ADUser -Filter "samAccountName -eq 'SampleS'"
+Get-ADUser -Identity SampleS
+# _____________________________________________________________________
 # Obtain AD user information, including SID
 
 $identity = Get-ADUser -Identity 'YOUR-USER'
@@ -5,7 +12,7 @@ $identity                     # Returns all information
 $identity.Name                # Returns the Name
 $identity.SID                 # Returns the SID
 $identity.UserPrincipalName   # Returns username@domain
-
+# _____________________________________________________________________
 # Obtain AD user information by using the SID
 # SID: S-X-X-XX-XXXXXXXXX-XXXXXXXXXX-XXXXXXX-XXXX
 $identity = Get-ADUser -Identity 'SID'
@@ -13,10 +20,10 @@ $identity                     # Returns all information
 $identity.Name                # Returns the Name
 $identity.SID                 # Returns the SID
 $identity.UserPrincipalName   # Returns username@domain
-
+# _____________________________________________________________________
 # Delete AD user:
 Remove-Aduser username -Confirm:$False    # Delete the username without confirmation
-                                          # Note: write username as is, no declaration
+# _____________________________________________________________________                                          # Note: write username as is, no declaration
 # Create AD user:
 $password = Read-Host -AsSecureString
 New-ADUser -Name "user" -Accountpassword $password -OtherAttributes @{'title'="worker";'mail'="user@company.com"} -PasswordNotRequired:$False
