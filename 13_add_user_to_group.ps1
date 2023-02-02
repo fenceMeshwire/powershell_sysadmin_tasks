b3c9207ff79a7963ab19717bd7f1b53d2a586445
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-02 23:14:14 +0100

    Update 13_add_user_to_group.ps1

--- 13_add_user_to_group.ps1
+++ 13_add_user_to_group.ps1
@@ -1,3 +1,5 @@
 # Add a user to a group
 
 dsmod group "CN=Sales,OU=Sales,OU=my_company,DC=my_domain,DC=org" -addmbr "CN=Sam Sample,OU=Sales,OU=my_company,DC=my_company,DC=org"
+
+Add-ADGroupMember -Identity "Sales" -Members SampleS
