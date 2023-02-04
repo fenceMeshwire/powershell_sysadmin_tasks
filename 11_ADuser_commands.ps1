4f3f10010a3b9de635e2f8021c31dae2f5a19b10
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-04 14:42:41 +0100

    Update 11_ADuser_commands.ps1

--- 11_ADuser_commands.ps1
+++ 11_ADuser_commands.ps1
@@ -1,10 +1,10 @@
 # List all AD users:
 Get-ADUser -Filter *
-# _____________________________________________________________________
+# _________________________________________________________________________________________
 # Search for a particular user "SampleS":
 Get-ADUser -Filter "samAccountName -eq 'SampleS'"
 Get-ADUser -Identity SampleS
-# _____________________________________________________________________
+# _________________________________________________________________________________________
 # Obtain AD user information, including SID
 
 $identity = Get-ADUser -Identity 'YOUR-USER' -Properties *
@@ -13,22 +13,30 @@ $identity.Name                # Returns the Name (common name)
 $identity.SID                 # Returns the SID
 $identity.UserPrincipalName   # Returns username@domain
 $identity.samAccountName      # Returns username
-# _____________________________________________________________________
+# _________________________________________________________________________________________
 # Obtain AD user information by using the SID
+# SID Decipherment:
 # SID: S-X-X-XX-XXXXXXXXX-XXXXXXXXXX-XXXXXXX-XXXX
+#      S:   Abbreviation for SID
+#      -X:  Revision number
+#        -X:  Identifier Authority
+#          -XX-XXXXXXXXX-XXXXXXXXXX-XXXXXXX:  local systems number
+#                                           -XXXX: user number; 500=Administrator;
+#                                                               1000+ = User
+#
 $identity = Get-ADUser -Identity 'SID'
 $identity                     # Returns all information
 $identity.Name                # Returns the Name
 $identity.SID                 # Returns the SID
 $identity.UserPrincipalName   # Returns username@domain
-# _____________________________________________________________________
+# _________________________________________________________________________________________
 # Show search results as table:
 Get-ADUser -Filter "Surname -eq 'Sample'" | Format-Table Surname, GivenName, samAccountName
 
 # Delete AD user:
 Remove-Aduser username -Confirm:$False    # Delete the username without confirmation
 #             Note: write username as is, no declaration
-# _____________________________________________________________________     
+# _________________________________________________________________________________________     
 # Create AD user:
 $password = Read-Host -AsSecureString
 New-ADUser -Name "user" -Accountpassword $password -OtherAttributes @{'title'="worker";'mail'="user@company.com"} -PasswordNotRequired:$False
@@ -45,5 +53,5 @@ $pwd = "13thWarrior!"
 
 # Command:
 dsadd user $attributes -samid $username -upn $upn -fn $fn -ln $ln -display $disp -mustchpwd no -pwd $pwd
-# _____________________________________________________________________   
+# _________________________________________________________________________________________   
 
