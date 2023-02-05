f009cf2c4cdd8a6245650c55dd44eab6d9a2507a
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-05 15:03:00 +0100

    Create 20_set_smb_shares.ps1

---
+++ 20_set_smb_shares.ps1
@@ -0,0 +1,13 @@
+# Set smb share:
+
+# Be sure to enable Services first, as follow:
+# 1. "C:\Windows\system32\svchost.exe -k LocalServiceAndNoImpersonation -p" on "AutomaticDelayedStart"
+# 2. "C:\Windows\System32\svchost.exe -k LocalServiceNetworkRestricted" on "AutomaticDelayedStart"
+
+# ________________________________________________________________________________________
+$Parameters = @{
+    Name = 'Data'
+    Path = 'C:\Data'
+}
+# Everybody is able to read the data within the directory above.
+New-SmbShare @Parameters
