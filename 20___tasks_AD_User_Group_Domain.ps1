6b186ea45257a7355a0aa2deb9fe7d6968a0f534
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-14 23:35:18 +0100

    Update 20___tasks_AD_User_Group_Domain.ps1

--- 20___tasks_AD_User_Group_Domain.ps1
+++ 20___tasks_AD_User_Group_Domain.ps1
@@ -1,1 +1,13 @@
+# Using dsadd command:
+# Setting up a AD group with multiple levels
+
+dsadd ou "ou=root,dc=domain_name,dc=com"
+dsadd ou "ou=first_level,ou=root,dc=domain_name,dc=com"
+dsadd ou "ou=second_level,ou=first_level,ou=root,dc=domain_name,dc=com"
+
+# Create and add user to AD group root
+
+dsadd user "cn=Sam Sample,ou=root,dc=domain_name,dc=com" `
+-samid SSample -upn SSample@domain_name.com -fn Sam -ln Sample -display "Sam Sample" `
+-desc "root commander" -mustchpwd no -pwdneverexpires yes -pwd "Pa$$w0rd!" 
 
