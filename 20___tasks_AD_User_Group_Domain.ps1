fe4ac2f716febd173f16c6c69216660843f169d6
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-14 23:36:19 +0100

    Update 20___tasks_AD_User_Group_Domain.ps1

--- 20___tasks_AD_User_Group_Domain.ps1
+++ 20___tasks_AD_User_Group_Domain.ps1
@@ -5,7 +5,7 @@ dsadd ou "ou=root,dc=domain_name,dc=com"
 dsadd ou "ou=first_level,ou=root,dc=domain_name,dc=com"
 dsadd ou "ou=second_level,ou=first_level,ou=root,dc=domain_name,dc=com"
 
-# Create and add user to AD group root
+# Create and add a user to AD group "root"
 
 dsadd user "cn=Sam Sample,ou=root,dc=domain_name,dc=com" `
 -samid SSample -upn SSample@domain_name.com -fn Sam -ln Sample -display "Sam Sample" `
