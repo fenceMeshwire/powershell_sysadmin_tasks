15b29e17f3d6f1ab70db3c44b9132c36e61a2959
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2023-02-15 10:17:29 +0100

    Update 03_disk_operations.ps1

--- 03_disk_operations.ps1
+++ 03_disk_operations.ps1
@@ -3,3 +3,5 @@
 Get-CimInstance -ClassName Win32_LogicalDisk
 
 Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,Size,FreeSpace
+
+Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'DiskFree (GB)'; Expression= { [float]($_.FreeSpace / 1GB) }}
