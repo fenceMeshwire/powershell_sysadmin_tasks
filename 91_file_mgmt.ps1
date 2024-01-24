f3e30887d50b9cc6a6d3a7f8d70e51f7337955c0
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2024-01-24 16:12:35 +0100

    Update 91_file_mgmt.ps1

--- 91_file_mgmt.ps1
+++ 91_file_mgmt.ps1
@@ -4,3 +4,6 @@ $Dir = get-childitem "\\Server\directory" -recurse -force
 $List = $Dir | where {$_.extension -eq ".xlsx"}
 
 $List | Select-Object fullname, LastAccessTime, LastWriteTime, CreationTime, @{N='Owner';E={$_.GetAccessControl().Owner}} | Export-Csv -path C:\Users\...\search_results.csv -NoTypeInformation
+
+# Export 
+Get-ChildItem -Path C:\xlsx_files -Filter *.xlsx -Recurse -ErrorAction SilentlyContinue -Force | Select-Object fullname, name, @{N='Owner';E={$_.GetAccessControl().Owner}} | Export-Csv -path C:\temp\xlsx_files.csv -NoTypeInformation
